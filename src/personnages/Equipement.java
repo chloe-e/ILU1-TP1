026586e6d2b75c86f7c7b381b80b63121470244e
chloe-e <chloeegodart@gmail.com>
2023-09-25 19:52:57 +0200

    4

---
+++ src/personnages/Equipement.java
@@ -0,0 +1,15 @@
+package personnages;
+
+public enum Equipement {
+	CASQUE ("casque"),BOUCLIER ("bouclier");
+	private String nom;
+
+	private Equipement(String nom) {
+		this.nom = nom;
+	}
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return super.toString();
+	}
+}
--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -3,11 +3,14 @@ package personnages;
 public class Romain {
 	private String nom;
 	private int force;
+	private Equipement[] equipement;
+	private int nbEquipement = 0;
 	
 	public Romain(String nom, int force) {
 		assert force>=0;
 		this.nom = nom;
 		this.force = force;
+		equipement = new Equipement[2];
 	}
 	public String getNom() {
 		return nom;
@@ -21,9 +24,7 @@ public class Romain {
 	public void recevoirCoup(int forceCoup) {
 		assert forceCoup>0;
 		int forceInit = this.force;
-		System.out.println(force);
 		this.force -= forceCoup;
-		System.out.println(forceInit);
 		if (this.force > 0) {
 			parler("Aïe !");
 		} else {
@@ -31,9 +32,38 @@ public class Romain {
 		}
 		assert forceInit>this.force;
 	}
+	public void sEquiper(Equipement equip) {
+		switch (nbEquipement) {
+		case 2:
+			System.out.println("Le romain "+this.nom+" est déjà bien protégé !");
+			break;
+		case 1:
+			if(equipement[0].equals(equip)) {
+				System.out.println("Le romain "+this.nom+" a déjà un " + equip+".");
+			}
+			else {
+				System.out.println("Le soldat "+this.nom+" s'équipe d'un " + equip+".");
+				equipement[nbEquipement]=equip;
+				nbEquipement+=1;
+			}
+			break;
+		default:
+			System.out.println("Le soldat "+this.nom+" s'équipe d'un " + equip+".");
+			equipement[nbEquipement]=equip;
+			nbEquipement+=1;
+			break;
+		}
+	}
 	public static void main(String[] args) {
 		Romain Cesar = new Romain("Cesar",6);
 		Cesar.parler("Bonjour je suis l'empereur des Romains.");
 		Cesar.recevoirCoup(10);
+		for(Equipement equip : Equipement.values()) {
+			System.out.println(equip);
+		}
+		Cesar.sEquiper(Equipement.CASQUE);
+		Cesar.sEquiper(Equipement.CASQUE);
+		Cesar.sEquiper(Equipement.BOUCLIER);
+		Cesar.sEquiper(Equipement.CASQUE);
 	}
 }
